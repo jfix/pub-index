4d2a4f50993f5bdbe54d2a75e56b5bf3f06e45b0
Couronne_R <Couronne_R@370d6576-d9c0-ee4c-9843-071cdce0ebce>
2013-03-12 16:48:01 +0000

    missed oecd-display.js (reformat only) in last commit!
    git-svn-id: https://pacps01.oecd.org/svn/publication-index/branches/dev-rc-grunt@299 370d6576-d9c0-ee4c-9843-071cdce0ebce

--- src/assets/js/oecd-display.js
+++ src/assets/js/oecd-display.js
@@ -1,34 +1,33 @@
 /*globals $*/
 $(function () {
-    
+
     /* load backlinks/related links */
     //var id = new RegExp("([^/])+(\\?|$)", "").exec(window.location.href)[0];
     //$('#backlinks').load('/app/actions/backlinks.xqy?id=' + id);
-    
     /* make TOC rows clickable: show/hide abstract */
     $('#toc-root').on('click', '.toc-row-header', function onClick_TocRowHeader(event) {
-        if (event.target.nodeName !== 'A') {
+        if(event.target.nodeName !== 'A') {
             var $target = $(this).find('a.icon-download-alt');
-            if ($target) {
+            if($target) {
                 var $row = $(this).parent('.toc-row');
                 $target = $row.find('.toc-abstract');
                 $target.fadeToggle();
             }
         }
     });
-    
+
     /* add logic to "show abstracts" & "show tables/graphs" action buttons */
     $('#toc-actions .btn').each(function () {
         var $btn = $(this);
         var toggle = $btn.data('toggle');
-        if (toggle) {
+        if(toggle) {
             var $targets = $('#toc-root').find(toggle);
             var $icon = $btn.children('i');
             $btn.click(function () {
                 var visible = $btn.hasClass('active');
-                
+
                 $btn.toggleClass('active');
-                if (visible) {
+                if(visible) {
                     $targets.fadeOut();
                 } else {
                     $targets.fadeIn();
@@ -36,20 +35,20 @@ $(function () {
             });
         }
     });
-    
+
     // don't show the big/small toc toggle button if there is no need
-    if ($("ul#toc-root").height() <= 400) {
-         $("#toc-expander").remove();
-         $("#toc-box").removeClass("toc-box-condensed");
+    if($("ul#toc-root").height() <= 400) {
+        $("#toc-expander").remove();
+        $("#toc-box").removeClass("toc-box-condensed");
     }
-    $("#toc-expander").on("click", function(evt) {
+    $("#toc-expander").on("click", function (evt) {
         var $btn = $(this);
         var $icn = $btn.find("i");
         var $lbl = $btn.find("span");
-        
+
         $("#toc-box").toggleClass("toc-box-condensed");
-        
-        if ( $icn.hasClass("icon-chevron-down")) {
+
+        if($icn.hasClass("icon-chevron-down")) {
             $icn.removeClass("icon-chevron-down");
             $icn.addClass("icon-chevron-up");
             $lbl.text("Reduce table of contents");
@@ -63,29 +62,25 @@ $(function () {
     /* add logic to summaries dropdown */
     $('#summaries').on('change', function onChange_Summaries() {
         var url = $(this).val();
-        if (url) {
+        if(url) {
             window.open(url);
         }
     });
     // show tooltip on mouseover to click to see abstract
     // FIXME: use .filter() instead, apparently faster
     //$("li.toc-row span.toc-title")
-      //  .tooltip({"delay": 100, "placement": "right", title: "Click to view abstract"});
-/* .toc-row:has(p.toc-abstract:hidden) */
+    //  .tooltip({"delay": 100, "placement": "right", title: "Click to view abstract"});
+    /* .toc-row:has(p.toc-abstract:hidden) */
     $("li.toc-row:has(p.toc-abstract:hidden)").hover(
-        function() {
-            $(this).find(".toc-row-header .toc-title").append("<strong class='click-abstract text-info'> Click to view abstract</strong>");
-                
-        },
-        function() {
-            $(this).find(".click-abstract").remove();
-            
-        });
-//        .tooltip({"delay": 100, "placement": "right", title: "!! Click to view abstract"});
 
+    function () {
+        $(this).find(".toc-row-header .toc-title").append("<strong class='click-abstract text-info'> Click to view abstract</strong>");
 
+    }, function () {
+        $(this).find(".click-abstract").remove();
 
-
+    });
+    //        .tooltip({"delay": 100, "placement": "right", title: "!! Click to view abstract"});
 
 
     /* enable tooltips on links in TOC */
